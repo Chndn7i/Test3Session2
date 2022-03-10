00f4ee7ca96e0b9f304e0cea1fab322914f32e15
Chandan Singh <97089654+Chndn7i@users.noreply.github.com>
2022-03-10 10:01:48 +0000

    nnnnn

--- p1final.c
+++ p1final.c
@@ -1,1 +1,33 @@
-#include <stdio.h>
+#include<stdio.h>
+int input_number()
+{
+  int n;
+  printf("Enter a number:\n");
+  scanf("%d",&n);
+  return n;
+}
+int is_composite(int n)
+{
+  int c=0,i;
+  for(i=1;i<=n;i++)
+    {
+      if(n % i == 0)
+        c++;
+    }
+  return c;
+}
+void output(int n,int composite)
+{
+  if(composite>2)
+   printf("%d is a composite number.",n);
+  else
+    printf("it is not a composite number.");
+}
+int main()
+{
+  int a,b;
+  a=input_number();
+  b=is_composite(a);
+  output(a,b);
+  return 0;
+  }
